61702db67caf95c9912231fd9349f2a2b4c69d53
Krzysztof Dobrzynski <k.dobrzynski94@gmail.com>
2017-10-17 18:42:35 +0200

    Cleaned up namespaces in IBiDictionary.cs

--- KD.Collections/IBiDictionary.cs
+++ KD.Collections/IBiDictionary.cs
@@ -3,14 +3,14 @@
 namespace KD.Collections
 {
     /// <summary>
-    /// Represents a generic <see cref="System.Collections.IDictionary"/> of keys/values which can be switched to values/keys.
+    /// Represents a generic <see cref="IDictionary{TKey, TValue}"/> of keys/values which can be switched to values/keys.
     /// </summary>
     /// <typeparam name="TKey"> The type of keys in the dictionary. </typeparam>
     /// <typeparam name="TValue"> The type of values in the dictionary. </typeparam>
     public interface IBiDictionary<TKey, TValue> : IDictionary<TKey, TValue>
     {
         /// <summary>
-        /// Switch Keys and Values and returns it as new <see cref="KD.Collections.IBiDictionary{TKey, TValue}"/>.
+        /// Switch Keys and Values and returns it as new <see cref="IBiDictionary{TKey, TValue}"/>.
         /// </summary>
         /// <returns> Returns new Dictionary with switched Keys and Values. </returns>
         IBiDictionary<TValue, TKey> Switch();
